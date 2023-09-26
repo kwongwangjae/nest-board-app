985cb1a29fc97505ee4eec43f2edcb3a6bd145f7
kwongwangjae <kgj9906@naver.com>
2023-09-27 03:25:24 +0900

    따라 배우는 NestJS 섹션 3

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -16,9 +25,29 @@ export class BoardsController {
 
   @Post()
   createBoard(
-    @Body('title') title: string,
-    @Body('description') description: string,
+    // @Body('title') title: string,
+    // @Body('description') description: string,
+    @Body() createBoardDto: CreateBoardDto,
   ): Board {
-    return this.boardsService.createBoard(title, description);
+    return this.boardsService.createBoard(createBoardDto);
+    // title, description
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
+  }
+
+  @Patch(':id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ) {
+    return this.boardsService.updateBoardStatus(id, status);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -9,8 +10,9 @@ export class BoardsService {
   getAllBoards(): Board[] {
     return this.boards;
   }
-
-  createBoard(title: string, description: string) {
+  //title: string, description: string
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
     const board: Board = {
       id: uuid(),
       title,
@@ -19,6 +21,20 @@ export class BoardsService {
     };
 
     this.boards.push(board);
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
+
+  updateBoardStatus(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+    board.status = status;
     return board;
   }
 }
