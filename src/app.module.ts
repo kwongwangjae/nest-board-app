fdb3156235ab02c0ab1881b85716cff7667cf2ca
kwongwangjae <kgj9906@naver.com>
2023-09-28 00:05:40 +0900

    따라 배우는 NestJs 섹션 5 typeorm 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
+import { typeORMConfig } from './boards/configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
 })
 export class AppModule {}
