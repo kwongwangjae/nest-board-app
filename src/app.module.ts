bf8a2b61e2854660fc01b159ed92607d99aad8fa
kwongwangjae <kgj9906@naver.com>
2023-10-06 14:01:24 +0900

    따라배우는 NestJs 섹션7 인증기능 구현하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,12 @@ import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './boards/configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { AuthController } from './auth/auth.controller';
-import { AuthService } from './—no-spec/auth/auth.service';
-import { AuthService } from './—no-spec/auth/auth.service';
+import { AuthService } from './auth/auth.service';
+import { UserRepository } from './auth/user.repository';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UserRepository], //UserRepository를 의존성주입을 위해서 넣었다.
 })
 export class AppModule {}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -6,19 +6,23 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+// import * as bcrypt from 'bcryptjs';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
-  async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
-    const { username, password } = authCredentialDto;
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    // const salt = await bcrypt.genSalt();
+    // const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({ username, password });
-    await this.save(user);
 
     try {
-      await user.save();
+      await this.save(user);
     } catch (error) {
       if (error.code === '23505') {
-        throw new ConflictException('중복되는 아이디 입니다.');
+        throw new ConflictException('Existing username');
       } else {
         throw new InternalServerErrorException();
       }
