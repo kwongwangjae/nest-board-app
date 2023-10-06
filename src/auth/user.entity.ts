88a7ec0114ebbf5a2b318f511d5f739986aca9d5
kwongwangjae <kgj9906@naver.com>
2023-10-06 12:35:11 +0900

    기본 유저 생성 구현

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -7,7 +7,7 @@ import {
 } from 'typeorm';
 
 @Entity()
-@Unique(['username'])
+@Unique(['username']) //중복이 일어나지 않게 해준다
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,27 @@
 import { CustomRepository } from 'src/module/typeorm-ex.decorator';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @CustomRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({ username, password });
+    await this.save(user);
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('중복되는 아이디 입니다.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
